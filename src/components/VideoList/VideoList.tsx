03d72d01313178547c960b5827b1d371ff3afad8
Michel-Hairun <michel.rak@hairun-technology.com>
2023-06-05 17:05:43 +0300

    set loader on fetch api

--- src/components/VideoList/VideoList.tsx
+++ src/components/VideoList/VideoList.tsx
@@ -3,27 +3,32 @@ import useVideoListLogic from './useLogic';
 import VideoListItem from './VideoListItem';
 import styles from './styles/videoContainer.module.less'
 import { VideoType } from '../../repositories/videoRepository';
+import Loader from '../Loader';
 
 const VideoList: React.FC = () => {
   const {
-    videoList
+    videoList,
+    loading
   } = useVideoListLogic()
 
   const handleClick = () => {
 
   }
 
-  return (
-    <div className={styles.videoContainer}>
-        {videoList.map((video: VideoType) => (
-          <VideoListItem
-            video={video}
-            key={`video-item-${video.id}`}
-            handleClick={handleClick}
-          />
-        ))}
-    </div>
-  )
+  return loading
+    ? (
+      <Loader />
+    ) : (
+      <div className={styles.videoContainer}>
+          {videoList.map((video: VideoType) => (
+            <VideoListItem
+              video={video}
+              key={`video-item-${video.id}`}
+              handleClick={handleClick}
+            />
+          ))}
+      </div>
+    )
 }
 
 VideoList.displayName = 'VideoListComponent'
