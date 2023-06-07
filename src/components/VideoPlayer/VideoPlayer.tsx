6b16e6c06b69db0bbbb954d49f6ff47702ea24b3
Michel-Hairun <michel.rak@hairun-technology.com>
2023-06-07 13:52:30 +0300

    add doc to video player component

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -5,6 +5,19 @@ import useVideoPlayerLogic from './useLogic';
 import {VideoPlayerBase} from '@enact/sandstone/VideoPlayer';
 import { IMAGE_DUMMY, VIDEO_DUMMY } from '../../constantes/data';
 import Container from '../ContainerSpotlight';
+
+/**
+ * There is to key down listener
+ * the first is when the video player appeard and focused
+ * we can listen to keydown without using video controll, 
+ * so we can check the stop keydown
+ * 
+ * the second is when we navigate on the button control
+ * we check if is stop keydown
+ * 
+ * in the two case we go back to main view
+ */
+
 const VideoPlayerComponent: React.FC = () => {
   const {
     videoToPlay,
